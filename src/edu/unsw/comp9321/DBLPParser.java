3de8f43850b3455714895f97aa952397233a4488
Edwin Kurniawan <kurniawanedwin95@gmail.com>
2016-09-01 02:18:20 +1000

    changed DBLPP to DBLPParser and modified that and IndexServlet with things i don't understand yet

--- src/edu/unsw/comp9321/DBLPParser.java
+++ src/edu/unsw/comp9321/DBLPParser.java
@@ -1,10 +1,33 @@
 package edu.unsw.comp9321;
 
 import javax.xml.parsers.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
-public class DBLPP {
+public class DBLPParser implements ContentHandler {
+	Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	private StringBuilder builder;
+	
+	public DBLPParser(InputStream xmlFile) throws ParsingFailedException {
+		try {
+			InputSource xmlSource = new InputSource(xmlFile);
+			XMLReader parser = XMLReaderFactory.createXMLReader();
+			
+			parser.setContentHandler(this);
+			parser.parse(xmlSource);
+		} catch(Exception e) {
+			logger.log(Level.SEVERE, "Failed to process XML File", e);
+			throw new ParsingFailedException(e);
+		}
+	}
 	
 	public static void main(String argv[]) {
 		
@@ -101,6 +124,73 @@ public class DBLPP {
 			e.printStackTrace();
 			
 		}
+		
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		logger.info("Adding characters to the buffer");
+		builder.append(new String(ch,start,length));
+		
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endPrefixMapping(String prefix) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processingInstruction(String target, String data) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDocumentLocator(Locator locator) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skippedEntity(String name) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startPrefixMapping(String prefix, String uri) throws SAXException {
+		// TODO Auto-generated method stub
 		
 	}
 
--- src/edu/unsw/comp9321/IndexServlet.java
+++ src/edu/unsw/comp9321/IndexServlet.java
@@ -1,8 +1,9 @@
 package edu.unsw.comp9321;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Vector;
-
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,11 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
  * Servlet implementation class IndexServlet
@@ -25,13 +31,29 @@ public class IndexServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    public void init() throws ServletException {
+    	super.init();
+    	
+    	InputStream is = getServletContext().getResourceAsStream("/WEB-INF/nijuuman.xml");
+    	DBLPParser parser;
+    	try {
+    		parser = new DBLPParser(is);
+    	} catch(ParsingFailedException e) {
+    		logger.severe("XML parsing failed"+e.getMessage());
+    		throw new ServletException(e);
+    	}
+    	
+    	//gotta make list of parsed stuff available for the entire app
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/result.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
